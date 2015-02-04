9bfd6caa39a72fdd17c9946134c3c3d619dcc7f5
Tyler Bules <Tylerbules@gmail.com>
2015-02-04 14:33:49 -0500

    opened serial channel on setup

--- TCU_FIRMWARE/TCU_FIRMWARE.ino
+++ TCU_FIRMWARE/TCU_FIRMWARE.ino
@@ -2,3 +2,19 @@
 #include <SPI.h>
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_GFX.h>
+
+void setup(void)
+{
+  //open up serial comms
+  //be careful with this, as this channel also connects to ESP8266
+  Serial.begin(9600);
+  Serial.println("Mr Thermostat");
+
+
+}
+
+void loop(void)
+{
+
+}
+
