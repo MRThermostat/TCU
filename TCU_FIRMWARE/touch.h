bd196f6651555d6dc08876e2e37e4fbe333c1491
Tyler Bules <Tylerbules@gmail.com>
2015-03-09 23:14:47 -0400

    not using defs in pins_arduino

--- TCU_FIRMWARE/touch.h
+++ TCU_FIRMWARE/touch.h
@@ -1,2 +1,3 @@
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300); //Replace 300 with actual resistance
+TouchScreen ts = TouchScreen(5, A1, A2, 4, 300); //Replace 300 with actual resistance
+//TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300); //Replace 300 with actual resistance
 
