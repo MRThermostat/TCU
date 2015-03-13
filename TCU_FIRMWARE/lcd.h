d2826fce16c13b7f941484154b470fd5eb21c90c
Chris Fasbinder <fasbincd.uc@gmail.com>
2015-03-13 16:19:15 -0400

    Changed lines 23, 27, 31, 35, 39
    Was assigning to x instead of comparing.

--- TCU_FIRMWARE/lcd.h
+++ TCU_FIRMWARE/lcd.h
@@ -20,23 +20,23 @@ bool readDiagnostics(){
   // read diagnostics (optional but can help debug problems)
   uint8_t x = tft.readcommand8(ILI9341_RDMODE);
   //Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
-  if(x = DISPLAY_POWER_MODE)
+  if(x == DISPLAY_POWER_MODE) 
   {
     x = tft.readcommand8(ILI9341_RDMADCTL);
     //Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
-    if(x = MADCTL_MODE)
+    if(x == MADCTL_MODE)
     {
       x = tft.readcommand8(ILI9341_RDPIXFMT);
       //Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
-      if(x = PIXEL_FORMAT)
+      if(x == PIXEL_FORMAT)
       {
         x = tft.readcommand8(ILI9341_RDIMGFMT);
         //Serial.print("Image Format: 0x"); Serial.println(x, HEX);
-        if(x = IMAGE_FORMAT)
+        if(x == IMAGE_FORMAT)
         {
           x = tft.readcommand8(ILI9341_RDSELFDIAG);
           //Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
-          if(x = SELF_DIAGNOSTIC)
+          if(x == SELF_DIAGNOSTIC)
           {
             return(1);
           }
